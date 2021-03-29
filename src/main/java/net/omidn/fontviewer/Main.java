e4bafe9da9f6abfbdf762877c26b8e5f18335dde
Omid.N <nejadabbasio@gmail.com>
2021-03-30 01:47:42 +0430

    Initial commit

---
+++ src/main/java/net/omidn/fontviewer/Main.java
@@ -0,0 +1,23 @@
+package net.omidn.fontviewer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("scene.fxml"));
+
+        Scene scene = new Scene(parent);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
